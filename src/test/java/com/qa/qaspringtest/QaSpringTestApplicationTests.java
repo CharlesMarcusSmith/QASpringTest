b1bfbca115d53feeb42c4cc1bd52d676b8d1c255
llamaladejam <charliemarcussmith@googlemail.com>
2022-07-05 09:42:17 +0100

    CS: Trying to upload project file to git

---
+++ src/test/java/com/qa/qaspringtest/QaSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.qaspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
